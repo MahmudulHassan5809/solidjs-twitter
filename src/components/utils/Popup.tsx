c5dfc45effbfa79f36b5a237f4c662f564c751fe
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2023-02-22 17:18:52 +0600

    feat: complete popup

--- src/components/utils/Popup.tsx
+++ src/components/utils/Popup.tsx
@@ -1,4 +1,11 @@
-import { Component, createSignal, Show } from "solid-js";
+import {
+  Component,
+  createEffect,
+  createSignal,
+  onCleanup,
+  onMount,
+  Show,
+} from "solid-js";
 
 type Props = {
   opener: Component;
@@ -7,13 +14,49 @@ type Props = {
 const Popup: Component<Props> = ({ opener: Opener }) => {
   const [isOpen, setIsOpen] = createSignal();
 
+  let followTo: HTMLDivElement;
+  let popup: HTMLDivElement;
+
+  onMount(() => {
+    window.addEventListener("click", closePopup);
+  });
+
+  onCleanup(() => {
+    window.removeEventListener("click", closePopup);
+  });
+
+  createEffect(() => {
+    if (isOpen()) {
+      if (isOpen()) {
+        adjustPopup();
+      }
+    }
+  });
+
+  const adjustPopup = () => {
+    popup.style.bottom = followTo.clientHeight + "px";
+  };
+
+  const closePopup = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div class="flex-it flex-grow">
-      <div onClick={() => setIsOpen(!isOpen())}>
+      <div
+        ref={followTo!}
+        onClick={(e) => {
+          e.stopImmediatePropagation();
+          setIsOpen(!isOpen());
+        }}
+      >
         <Opener />
       </div>
       <Show when={isOpen()}>
-        <div class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000">
+        <div
+          ref={popup!}
+          class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000"
+        >
           <div class="w-72 min-w-68 max-h-120 min-h-8 flex-it overflow-auto">
             <div class="flex-it flex-grow flex-shrink py-3">
               <div class="flex-it px-4 py-3 transition hover:bg-gray-700">
