58b89f91ceb5a6d23eb77a7d36e19c16f491768b
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2023-02-22 15:54:24 +0600

    feat: create different screens

---
+++ src/screens/Login.tsx
@@ -0,0 +1,8 @@
+import { Component } from "solid-js";
+import MainLayout from "../components/layouts/Main";
+
+const LoginScreen: Component = () => {
+  return <MainLayout>Hello I am LoginScreen</MainLayout>;
+};
+
+export default LoginScreen;
---
+++ src/screens/Register.tsx
@@ -0,0 +1,8 @@
+import { Component } from "solid-js";
+import MainLayout from "../components/layouts/Main";
+
+const RegisterScreen: Component = () => {
+  return <MainLayout>Hello I am RegisterScreen</MainLayout>;
+};
+
+export default RegisterScreen;
